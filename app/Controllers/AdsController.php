8f2d3cda6fb99f869de113b2a39a658e21183588
Oleg Shmakov <flame.in.the.head@gmail.com>
2022-01-29 17:27:17 +0300

    Add AdsController

---
+++ app/Controllers/AdsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+class AdsController
+{
+    public function add()
+    {
+        echo "ADD OK";
+    }
+
+    public function relevant()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+}

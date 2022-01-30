6354c5b14e925688ae48ec71fdd2cc234e5dde4c
Oleg Shmakov <flame.in.the.head@gmail.com>
2022-01-30 15:37:37 +0300

    Output only specified fields.

--- app/Entity/Ads.php
+++ app/Entity/Ads.php
@@ -16,31 +16,53 @@ class Ads
     }
 
     // проверка на дубль
-    public function checkExists($data)
+    public function checkExists($ads)
     {
-        return !empty($this->storage->getHash($this->getStorageKey($data)));
+        return !empty($this->storage->getHash($this->getStorageKey($ads)));
     }
 
-    public function add($data)
+    public function add($ads)
     {
         $lastId = $this->storage->get('lastId');
         if(empty($lastId)){
             $lastId = 0;
         }
 
-        $storageKey = $this->getStorageKey($data);
-        $data['id'] = ++$lastId;
-        if($this->storage->setHash($storageKey, $data)){
+        $storageKey = $this->getStorageKey($ads);
+        $ads['id'] = ++$lastId;
+        if($this->storage->setHash($storageKey, $ads)){
             $this->storage->set('lastId', $lastId);
-            $this->storage->set($data['id'], $storageKey);
-            return $data;
+            $this->storage->set($ads['id'], $storageKey);
+            return $this->checkOnlyFields($ads);
         }
 
         return false;
     }
 
-    private function getStorageKey($data)
+    // ключ для хранилища
+    private function getStorageKey($ads)
     {
-        return md5(serialize($data));
+        return md5(serialize($ads));
+    }
+
+    // ключи, которые отдают клиенту
+    private function responseOnly()
+    {
+        return ['id', 'text', 'banner'];
+    }
+
+    // фильтрация лишних полей и сортировка, согласно их порядку
+    private function checkOnlyFields($ads)
+    {
+        if(!$onlyFields = $this->responseOnly()){
+            return $ads;
+        }
+
+        $output = [];
+        foreach($onlyFields as $fieldName){
+            $output[$fieldName] = $ads[$fieldName];
+        }
+
+        return $output;
     }
 }
